29268904b0ef1430721c083596924af04851685c
profit0124 <profit0124@gmail.com>
2024-01-02 22:48:02 +0900

    수리공항승 실패...

--- AlgorithmEx/main.swift
+++ AlgorithmEx/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+
+// MARK: 수리공 항승
+let firstInput = readLine()!.components(separatedBy: " ")
+let secondInput = readLine()!.components(separatedBy: " ")
+let total = Int(firstInput.first!)!
+let distance = Double(firstInput.last!)!
+let array = secondInput.map { Double($0)! }
+
+var result = total
+
+check(array: array, count: 0, distance: distance, minVal: 0, maxVal: 0)
+print(result)
+
 
---
+++ AlgorithmEx/수리공 항승.swift
@@ -0,0 +1,32 @@
+//
+//  수리공 항승.swift
+//  AlgorithmEx
+//
+//  Created by Sooik Kim on 1/2/24.
+//
+
+import Foundation
+
+
+
+func check(array: [Double], count: Int, distance: Double, minVal: Double, maxVal: Double) {
+    var temp = array
+    let base = temp.removeFirst()
+    let startPoint = base - 0.5
+    let endPoint = base + 0.5
+    
+    if minVal <= startPoint, maxVal >= endPoint {
+        if temp.isEmpty {
+            result = min(count, result)
+        } else {
+            check(array: temp, count: count, distance: distance, minVal: minVal, maxVal: maxVal)
+            check(array: temp, count: count + 1, distance: distance, minVal: startPoint, maxVal: startPoint + distance)
+        }
+    } else {
+        if temp.isEmpty {
+            result = min(count + 1, result)
+        } else {
+            check(array: temp, count: count + 1, distance: distance, minVal: startPoint, maxVal: startPoint + distance)
+        }
+    }
+}
